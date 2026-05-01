e2008f24718066628089f00a6efa3ddceabbe77e
Salony Ranjan <salonyranjan@gmail.com>
2026-05-02 01:00:32 +0530

    Update image remote patterns in next.config.js

--- next.config.js
+++ next.config.js
@@ -2,26 +2,27 @@
 const nextConfig = {
   images: {
     remotePatterns: [
-      { protocol: "https", hostname: "m.media-amazon.com" },
-      { protocol: "https", hostname: "static.wikia.nocookie.net" },
-      { protocol: "https", hostname: "flxt.tmsimg.com" },
-      { protocol: "https", hostname: "i.pinimg.com" },
-      { protocol: "https", hostname: "cdn.myanimelist.net", pathname: "/images/anime/**" },
+      { 
+        protocol: "https", 
+        hostname: "i.pinimg.com", 
+        pathname: "/**" 
+      },
+      { 
+        protocol: "https", 
+        hostname: "cdn.myanimelist.net", 
+        pathname: "/images/anime/**" 
+      },
       {
         protocol: 'https',
         hostname: 'onepunchman-scans.online',
-        pathname: '/**', // Allows all paths from this host
-      },
-      {
-        protocol: 'https',
-        hostname: 'i.pinimg.com',
         pathname: '/**',
       },
-      ],
+    ],
   },
   experimental: {
     serverActions: {
-      allowedOrigins: ["localhost:3000"],
+      // Limit actions to your local dev environment for now
+      allowedOrigins: ["localhost:3000"], 
     },
   },
 };
